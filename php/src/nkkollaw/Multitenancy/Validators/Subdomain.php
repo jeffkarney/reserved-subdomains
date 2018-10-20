db4c2cde465b2ee3ec4feeedc853233eeb524cf3
Jeff Karney <jeff.karney@gmail.com>
2018-10-20 10:17:51 -0500

    Add ability to pass in additional files and subdomains
    Additional YAML files can now be passed in via an array - keeps backwards compatibility of single string
    
    Array(s) of additional reserved subdomains can also be passed in
    
    If the $reserved_lists var is a string, it will work as in the past
    
    If the $reserved_lists var is an array, it will be scanned for stings and arrays, strings are additional YAML files while arrays are additional reserved subdomains to be checked
    
    By default, the default YAML file will automatically be included, set $ignore_default_list to true if you do not want to also use the default
    
    //will add these additional reserved subdomains to the list ('save','edit','delete','my-custom-subdomain1')
    isReserved('test-subdomain', [['save','edit','delete','my-custom-subdomain1']]);
    
    //will only check these reserved subdomains ('save','edit','delete','my-custom-subdomain1')
    isReserved('test-subdomain', [['save','edit','delete','my-custom-subdomain1']], true);
    
    //will add these additional reserved subdomains to the list ('save','edit','delete','my-custom-subdomain1')
    //will add an additional YAML file
    isReserved('test-subdomain', ['additional-reserved-subdomains.yaml', ['save','edit','delete','my-custom-subdomain1']]);

--- php/src/nkkollaw/Multitenancy/Validators/Subdomain.php
+++ php/src/nkkollaw/Multitenancy/Validators/Subdomain.php
@@ -1,49 +1,71 @@
 <?php
+
 namespace nkkollaw\Multitenancy\Validators;
+
 class Subdomain
 {
+	public static function getReservedSubdomains($reserved_lists, $ignore_default_list)
+	{
+		if (is_array($reserved_lists)) {
+			if (!$ignore_default_list) {
+				$reserved_suddomains[] = __DIR__ . '/../../../../../reserved-subdomains.yaml';
+			}
+			$reserved_subdomains = [[]];
+			foreach ($reserved_lists as $reserved_list) {
+				if (is_array($reserved_list)) {
+					$reserved_subdomains[] = $reserved_list;
+				} else {
+					$reserved_subdomains[] = self::readYAML($reserved_lists);
+				}
+			}
 
-    public static function readYAML($yaml_file)
-    {
-        $yaml = file_get_contents($yaml_file);
-        if (!$yaml) {
-            throw new \Exception('unable to find YAML file');
-        }
+			return array_merge(...$reserved_subdomains);
+		}
 
-        $reserved_subdomains = \Symfony\Component\Yaml\Yaml::parse($yaml);
-        if (!$reserved_subdomains) {
-            throw new \Exception('unable to parse YAML');
-        }
+		return self::readYAML($reserved_lists);
+	}
 
-        return $reserved_subdomains;
-    }
+	public static function readYAML($yaml_file)
+	{
+		$yaml = file_get_contents($yaml_file);
+		if (!$yaml) {
+			throw new \Exception('unable to find YAML file');
+		}
 
-    public static function isRegex($str)
-    {
-        return strpos($str, '/') !== false;
-    }
+		$reserved_subdomains = \Symfony\Component\Yaml\Yaml::parse($yaml);
+		if (!$reserved_subdomains) {
+			throw new \Exception('unable to parse YAML');
+		}
 
-    public static function isReserved($subdomain, $yaml_file = __DIR__ . '/../../../../../reserved-subdomains.yaml')
-    {
-        $reserved_subdomains = self::readYAML($yaml_file);
+		return $reserved_subdomains;
+	}
 
-        $is_reserved = false;
-        foreach ($reserved_subdomains as $reserved_subdomain) {
-            if (self::isRegex($reserved_subdomain)) {
-                if (preg_match($reserved_subdomain, $subdomain)) {
-                    $is_reserved = true;
+	public static function isRegex($str)
+	{
+		return strpos($str, '/') !== false;
+	}
 
-                    break;
-                }
-            } else {
-                if ($reserved_subdomain == $subdomain) {
-                    $is_reserved = true;
+	public static function isReserved($subdomain, $reserved_lists = [], $ignore_default_list = false)
+	{
+		$reserved_subdomains = self::getReservedSubdomains($reserved_lists);
 
-                    break;
-                }
-            }
-        }
+		$is_reserved = false;
+		foreach ($reserved_subdomains as $reserved_subdomain) {
+			if (self::isRegex($reserved_subdomain)) {
+				if (preg_match($reserved_subdomain, $subdomain)) {
+					$is_reserved = true;
 
-        return $is_reserved;
-    }
+					break;
+				}
+			} else {
+				if ($reserved_subdomain == $subdomain) {
+					$is_reserved = true;
+
+					break;
+				}
+			}
+		}
+
+		return $is_reserved;
+	}
 }
